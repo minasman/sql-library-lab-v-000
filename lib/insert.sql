ad8778a4bcbf5558da57a4d6cfca58650f70919d
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-06-24 03:56:00 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,9 +1,11 @@
 INSERT INTO series (id, title, author_id, subgenre_id) VALUES (1, "Game of Thrones", 1, 1);
 INSERT INTO series (id, title, author_id, subgenre_id) VALUES (2, "Harry Potter", 2, 2);
 
-INSERT INTO subgenres () VALUES ();
+INSERT INTO subgenres (id, name) VALUES (1, "Medieval Fantasy");
+INSERT INTO subgenres (id, name) VALUES (2, "Witches");
 
-INSERT INTO authors () VALUES ();
+
+INSERT INTO authors (id, name) VALUES ();
 
 INSERT INTO books () VALUES ();
 

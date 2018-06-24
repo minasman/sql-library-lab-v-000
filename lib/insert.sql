ab4f129a5fb382d885cba27384a4cb5541042886
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-06-24 04:00:05 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -9,6 +9,14 @@ INSERT INTO authors (id, name) VALUES (1, "Dan Brown");
 INSERT INTO authors (id, name) VALUES (2, "Jane Doe");
 
 
-INSERT INTO books (id, title, year, series_id) VALUES ();
+INSERT INTO books (id, title, year, series_id) VALUES (1, "Game of Thrones", 2000, 1);
+INSERT INTO books (id, title, year, series_id) VALUES (2, "Rise of Kings", 2002, 1);
+INSERT INTO books (id, title, year, series_id) VALUES (3, "Demise of Queens", 2004, 1);
 
-INSERT INTO characters () VALUES ();
+INSERT INTO books (id, title, year, series_id) VALUES (4, "Harry Potter", 2001, 2);
+INSERT INTO books (id, title, year, series_id) VALUES (5, "The Witcher Warnings", 2003, 2);
+INSERT INTO books (id, title, year, series_id) VALUES (6, "The Sorcerers Stone", 2005, 2);
+
+
+
+INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES ();
